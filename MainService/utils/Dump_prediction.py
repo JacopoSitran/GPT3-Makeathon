02d813bca3b38e02c647e80147d65fade3723887
NotAlessandroBeatini <82681229+NotAlessandroBeatini@users.noreply.github.com>
2021-04-18 00:26:53 +0200

    Update Dump_prediction.py

--- MainService/utils/Dump_prediction.py
+++ MainService/utils/Dump_prediction.py
@@ -5,7 +5,7 @@ path = r'C:\Users\Ale\Downloads\GPT-3-Hackaton-Dataset-handout(2).xlsx'
 def dump_on_excel(json,index=2,PATH=path):
 
     file = openpyxl.load_workbook(PATH)
-    ind = "A"+str(index)
+    ind = "G"+str(index)
     sheet =  file.active
     sheet[ind] = json
     file.save(path)
