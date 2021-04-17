998ee450ce603e69ae5a6d629c7a00f9df9980ab
NotAlessandroBeatini <82681229+NotAlessandroBeatini@users.noreply.github.com>
2021-04-18 00:26:32 +0200

    Add files via upload
    Dump on excel. takes as argument (json,index of Gcolumn,path of the excel file).   
    THERE ARE PERMISSION OF ACCESS ISSUES

---
+++ MainService/utils/Dump_prediction.py
@@ -0,0 +1,11 @@
+import openpyxl
+
+path = r'C:\Users\Ale\Downloads\GPT-3-Hackaton-Dataset-handout(2).xlsx'
+
+def dump_on_excel(json,index=2,PATH=path):
+
+    file = openpyxl.load_workbook(PATH)
+    ind = "A"+str(index)
+    sheet =  file.active
+    sheet[ind] = json
+    file.save(path)
