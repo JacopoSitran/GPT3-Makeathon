b5e4086368e1341d3e448836da596072b48ea26e
arnor <7689495+arnorkr@users.noreply.github.com>
2021-04-17 20:41:07 +0200

    create text response from json

--- MainService/utils/email_service.py
+++ MainService/utils/email_service.py
@@ -15,7 +15,7 @@ def send_message(message):
 
 
 async def process_new_messages(task):
-    
+
     messages = []
     mail = imaplib.IMAP4_SSL(SERVER)
     mail.login(EMAIL, PASSWORD)
@@ -24,10 +24,10 @@ async def process_new_messages(task):
     (retcode, messages) = mail.search(None, '(UNSEEN)')
     if retcode == 'OK':
         for num in messages[0].split() :
-            typ, data = mail.fetch(num,'(RFC822)')      
+            typ, data = mail.fetch(num,'(RFC822)')
             raw_email = data[0][1]
             _typ, _data = mail.store(num,'+FLAGS','\\Seen')
-            raw_email_string = raw_email.decode('utf-8')              
+            raw_email_string = raw_email.decode('utf-8')
             email_message = email.message_from_string(raw_email_string)
             for part in email_message.walk():
                 if part.get_content_type() == "text/plain":
@@ -38,3 +38,21 @@ async def process_new_messages(task):
                         continue
     print(messages)
     return messages
+
+def create_email_response_from_orders(orders_json):
+    # todo make GPT-3 summarize order email
+    return '\n\n'.join([summarize(order) for order in orders_json["orders"]] + ['Vielen dank'])
+
+def summarize(order):
+    positions = ", ".join(order["Positions"])
+    is_open = order.get("Action", "Close") == "Open"
+
+    resp = [
+    f'Die Bestellung mit Ordnungsnummer {order["PO"]} hat jetzt folgende Positionen: {positions}.']
+    if "elk" in order["Type"]:
+        resp.append('Endlieferung wurde gesetzt.')
+    if "erk" in order["Type"]:
+        resp.append('Endrechnung wurde gesetzt.')
+    resp.append('Die Bestellung ist immer noch offen.' if is_open else 'Bestellung ist jetzt geschlossen.')
+
+    return '\n'.join(resp)
